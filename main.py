3bb34baf96aa509078753da4b5b625248045f2fe
Shubham89Mahajan <mahajanshubham89@gmail.com>
2023-09-02 11:57:52 +0530

    Added artifact and config and Data Ingestion functionality

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.utils import get_collection_as_dataframe
 import sys,os
+from sensor.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
+from sensor.components import data_ingestion
 
 def test_logger_and_exception():
      try:
@@ -16,6 +18,11 @@ def test_logger_and_exception():
 
 if __name__=="__main__":
      try:
-          get_collection_as_dataframe(database_name="aps", collection_name="sensor")
+          #get_collection_as_dataframe(database_name="aps", collection_name="sensor")
+          training_pipeline_config = TrainingPipelineConfig()
+          data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
-          print(e)
+          raise SensorException(e, sys)
